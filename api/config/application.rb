64294d6869e66605e6de4fc9823dc7a7a19c946e
Lucas Winningham <lucas.winningham@gmail.com>
2019-06-17 18:32:50 -0400

    api: get dotenv to actually load

--- api/config/application.rb
+++ api/config/application.rb
@@ -16,6 +16,8 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
