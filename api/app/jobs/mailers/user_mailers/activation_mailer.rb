7f183735d0871af06ab1e1129b55c6c56660e861
Lucas Winningham <lucas.winningham@gmail.com>
2019-07-14 16:50:34 -0400

    api: fill out activation mailer file

---
+++ api/app/jobs/mailers/user_mailers/activation_mailer.rb
@@ -0,0 +1,31 @@
+module Mailers
+  module UserMailers
+    class ActivationMailer
+      include Job
+
+      def initialize(user)
+        @user = user
+      end
+
+      def deliver
+        enqueue queue: queue, payload: payload
+      end
+
+      private
+
+      attr_reader :user
+
+      def queue
+        'mailers.user.activation'
+      end
+
+      def payload
+        { email: user.email, activation_link: activation_link }
+      end
+
+      def activation_link
+        "#{ENV['CLIENT_URL']}/activations/#{user.name}/#{user.activation.token}"
+      end
+    end
+  end
+end
