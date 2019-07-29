6daa8dff554dd93a7e08a491165dee78975cfddd
Lucas Winningham <lucas.winningham@gmail.com>
2019-07-28 22:07:29 -0400

    mailer: clean up config

--- mailer/lib/mailer/env.rb
+++ mailer/lib/mailer/env.rb
@@ -1,11 +1,25 @@
+require 'dotenv/load'
+
 module Mailer
   class Env
     def self.production?
       ENV['ENVIRONMENT'] == 'production'
     end
 
+    def self.gmail_username
+      ENV['GMAIL_USERNAME']
+    end
+
+    def self.gmail_password
+      ENV['GMAIL_PASSWORD']
+    end
+
+    def self.amqp_url
+      ENV['AMQP_URL']
+    end
+
     def self.app_email
-      "#{ENV['GMAIL_USERNAME']}@gmail.com"
+      "#{gmail_username}@gmail.com"
     end
   end
 end
