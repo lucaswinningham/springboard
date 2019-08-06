27c8bf5266691e353aba79c56369c39cb0ccda46
Lucas Winningham <lucas.winningham@gmail.com>
2019-08-05 22:53:47 -0400

    client: set up for api change

--- client/src/app/services/queries/user-signup.service.ts
+++ client/src/app/services/queries/user-signup.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { ApolloQueryResult } from 'apollo-client';
+import { map, mergeMap } from 'rxjs/operators';
 
 import { ApiService } from '@services/utils/api.service';
 
 import { UserAuth } from '@app/models/user/user-auth.model';
 
-export const document = `
+export const userSignupDocument = `
   mutation userSignup($name: String! $email: String!) {
     userSignup(name: $name email: $email) {
       salt nonce ckey civ
+    }
+  }
+`;
+
+export const userPasswordChangeDocument = `
+  mutation userPasswordChange($email: String! $prevPassword: String! $nextPassword: String!) {
+    userPasswordChange(email: $email prevPassword: $prevPassword nextPassword: $nextPassword) {
+      jwt
     }
   }
 `;
@@ -23,9 +29,28 @@ export const document = `
 export class UserSignupService {
   constructor(private api: ApiService) { }
 
-  public signup(variables: { name: string, email: string }): Observable<UserAuth> {
-    return this.api.mutate({ document, variables }).pipe(
+  public signup(args: { name: string, email: string, password: string }): Observable<UserAuth> {
+    const { name, email, password } = args
+
+    const userSignupVariables = { name, email };
+    const userSignup$ = this.api.mutate(
+      { document: userSignupDocument, variables: userSignupVariables }
+    ).pipe(
       map(queryResult => new UserAuth(queryResult.data.userSignup))
+    );
+
+    const userPasswordChangeVariables = { email, prevPassword: null, nextPassword: password };
+    const userPasswordChange$ = this.api.mutate(
+      { document: userPasswordChangeDocument, variables: userPasswordChangeVariables }
+    ).pipe(
+      map(queryResult => new Jwt(queryResult.data.jwt))
+    )
+
+    return userSignup$.pipe(
+      mergeMap(userAuth => {
+        const nextPassword = this.passwordService.doSomething({ userAuth, password });
+        return userPasswordChange$;
+      })
     );
   }
 }
